dc05695646a94c4cbbd4c5e69ee3f834fb196795
Eitheldan <eithel57@gmail.com>
2022-01-18 14:57:50 +0100

    Ajout Fonctions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,16 @@
   <div>
     <Header />
     <Products />
-    <!-- <Products2 /> -->
   </div>
 </template>
 
 <script>
 import Header from "../components/Header.vue";
 import Products from "../components/Products.vue";
-import Products2 from "../components/Products2.vue";
 
 export default {
   name: "Home",
-  components: { Header, Products, Products2 },
+  components: { Header, Products },
 };
 </script>
 
