86cb4edba92a75992644332364a32f0c5afbf2f2
Vincent Lescane <vincentlse34@gmail.com>
2022-01-19 17:28:38 +0100

    pages admin v1

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import ProductDetails from '../views/ProductDetails.vue'
 import CardDetails from '../views/CardDetails.vue'
+import admin from '../views/admin_boutique.vue'
+import association from '../views/association.vue'
+import commande from '../views/commande.vue'
+import compteBoutique from '../views/compteBoutique.vue'
+import compteClient from '../views/compteClient.vue'
+import logout from '../views/logout.vue'
+import produits from '../views/produits.vue'
+import stock from '../views/stock.vue'
+import tva from '../views/tva.vue'
 
 const routes = [
   {
@@ -18,7 +27,53 @@ const routes = [
     path: '/cardDetails',
     name: 'CardDetails',
     component: CardDetails
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component : admin
+  },
+  {
+    path: '/admin/association',
+    name: 'association',
+    component : association
+  },
+  {
+    path: '/admin/commande',
+    name: 'commande',
+    component : commande
+  },
+  {
+    path: '/admin/compteBoutique',
+    name: 'compteBoutique',
+    component : compteBoutique
+  },
+  {
+    path: '/admin/compteClient',
+    name: 'compteClient',
+    component : compteClient
+  },
+  {
+    path: '/admin/logout',
+    name: 'logout',
+    component : logout
+  },
+  {
+    path: '/admin/produits',
+    name: 'produits',
+    component : produits
+  },
+  {
+    path: '/admin/stock',
+    name: 'stock',
+    component : stock
+  },
+  {
+    path: '/admin/tva',
+    name: 'tva',
+    component : tva
   }
+  
 ]
 
 const router = createRouter({
